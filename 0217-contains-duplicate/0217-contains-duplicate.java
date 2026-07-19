5519243e4a8420d2395c06255f5b0efe65cc15b8
AVNEESH KUMAR <dev.avneeshk@gmail.com>
2026-07-19 11:32:43 +0530

    Time: 19 ms (39.75%), Space: 108.4 MB (12.24%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,15 +1,13 @@
+import java.util.HashSet;
 class Solution{
     public boolean containsDuplicate(int[] nums){
-        HashMap<Integer, Integer> store = new HashMap<>();
-        for(int i = 0; i<nums.length; i++){
-            if(store.containsKey(nums[i])){
-                return true;
+        HashSet<Integer> seen = new HashSet<>();
+        for(int num: nums){
+            if(seen.contains(num)){
+                return true ; 
             }
-            store.put(nums[i], i);
-        } 
-        return false;
+            seen.add(num);
 
+        }return false ;
     }
-    
-    
 }
